a38256be7b2596a39116b3a2152ec40b84803aa8
Ravinshu Makkar <55399637+mravinshu@users.noreply.github.com>
2022-12-31 23:33:31 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
+import os
 from flask import Flask
-from flask_socketio import SocketIO, emit, join_room, send, rooms
+from flask_socketio import SocketIO, emit, join_room, send, leave_room
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'RandomSecretKey'
-socketio = SocketIO(app, cors_allowed_origins="*")
+socketio = SocketIO(app, cors_allowed_origins="*", logging=True, engineio_logger=True)
+
+
+@app.route('/')
+def index():
+    return {'message': 'Hello World'}
 
 
 @socketio.on('my event')
@@ -20,7 +26,19 @@ def handle_message(message):
 @socketio.on('message_to')
 def handle_message_to(message):
     message = eval(message)
-    emit('message', message.get('message'), room=message.get('room'), include_self=False)
+    print('received message: ' + str(message))
+    returnString = '{"message": "' + message.get('message') + '","username": "' + message.get('username') + '"}'
+    print(returnString)
+    emit('message', returnString, room=message.get('room'), include_self=False)
+
+
+@socketio.on('leave')
+def handle_leave(data):
+    data = eval(data)
+    leave_room(data.get('room'))
+    room = data.get('room')
+    return_string = '{"message": "' + data.get('username') + ' has left the room","username": "Server"}'
+    emit('message', return_string, room=room)
 
 
 @socketio.on('join')
@@ -29,9 +47,13 @@ def on_join(data):
     username = data.get('username')
     room = data.get('room')
     join_room(room)
+    print('user ' + username + ' joined room ' + room)
+    returnString = '{"message": "' + 'user ' + username + ' joined room ' + room + '","username": "Server"}'
     # emit('my response', {'data': 'In rooms: ' + ', '.join(rooms())}, room=room)
-    send(username + ' has entered the room.', room=room)
+    send(returnString, room=room, include_self=False)
 
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    port = int(os.environ.get('PORT', 4000))
+    print('Running on port ' + str(port))
+    socketio.run(app, debug=True, port=port, host='0.0.0.0')
